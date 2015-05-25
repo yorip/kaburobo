caa3b46a82ae3f6ed876f513fcfef41e3436c07e
AkihiroYorita <younglion3@yahoo.co.jp>
2015-05-25 19:01:41 +0900

    最初のロボット

---
+++ kaburobo-sdk/robot/FirstRobot.java
@@ -0,0 +1,19 @@
+import jp.tradesc.superkaburobo.sdk.robot.AbstractRobot;
+import jp.tradesc.superkaburobo.sdk.trade.TradeAgent;
+
+
+public class FirstRobot extends AbstractRobot {
+
+	@Override
+	public void order(TradeAgent arg0) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void screening(TradeAgent arg0) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
